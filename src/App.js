490a0b1dade60397f5a6806e830477bf05bfdb24
Xaver <info@xaverhirsch.de>
2020-05-18 12:14:25 +0200

    add styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
+import styled from "styled-components";
 
+///////////////////////////////////////
+// SOCKET CONNECTION
 const socket = io("https://dreamsynth-server.herokuapp.com/");
 
 socket.on("connect", () => {
@@ -12,6 +15,24 @@ socket.on("connect", () => {
 ///////////////////////////////////////
 // APP
 
+const StyledApp = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: black;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  & h1 {
+    text-align: center;
+    color: white;
+    font-size: 5rem;
+  }
+`;
+
 const App = () => {
   const [msg, setMsg] = useState("");
 
@@ -22,9 +43,9 @@ const App = () => {
   });
 
   return (
-    <div>
+    <StyledApp>
       <h1>{msg}</h1>
-    </div>
+    </StyledApp>
   );
 };
 
